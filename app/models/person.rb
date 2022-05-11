127945f4077d9a33e6491ea4b4fafad42f70f56c
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2022-05-11 17:22:26 -0300

    fix: associations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Person < ApplicationRecord
+  # rubocop:disable Metrics/LineLength
   belongs_to :father, class_name: "Person", optional: true
   belongs_to :mother, class_name: "Person", optional: true
   belongs_to :spouse, class_name: "Person", optional: true
@@ -8,10 +9,10 @@ class Person < ApplicationRecord
 
   has_many :citations, inverse_of: :person, dependent: :destroy
   has_many :archive_citation, through: :citations, source: :record, source_type: "Archive"
-
   has_many :donated_archives, as: :donor, class_name: "Archive", inverse_of: :donor, dependent: :restrict_with_error
-  has_many :interviews_given, foreign_key: "interviewed_id", inverse_of: :interviewed, dependent: :destroy
-  has_many :interviews_made, foreign_key: "interviewer_id", inverse_of: :interviewer, dependent: :destroy
+  has_many :interviews_given, class_name: "Interview", foreign_key: "interviewed_id", inverse_of: :interviewed, dependent: :destroy
+  has_many :interviews_made,  class_name: "Interview", foreign_key: "interviewer_id", inverse_of: :interviewer, dependent: :destroy
+  # rubocop:enable Metrics/LineLength
 
   has_one_attached :pdf
   has_many_attached :images
