69879f3cc24253f188b6c62f734b32a4fb9c84d8
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2022-06-16 20:08:34 +0200

    feat: add location search api

--- app/assets/javascripts/tom_select_init.js
+++ app/assets/javascripts/tom_select_init.js
@@ -1,6 +1,35 @@
 function tomSelectInit(selector = ".tom-select-init") {
   document.querySelectorAll(selector).forEach((el) => {
-    const settings = {};
+    const settings = el.classList.contains("location-tom-select") ? locationFetchSettings() : {};
     new TomSelect(el, settings);
   });
+}
+
+function locationFetchSettings() {
+  return {
+    valueField: "display_name",
+    labelField: "display_name",
+    searchField: "display_name",
+    load: function (query, callback) {
+      params = {
+        email: "leer@usp.br",
+        format: "jsonv2",
+        limit: 5,
+        "accept-language": "pt-BR",
+        q: query,
+      };
+
+      const query_params = [];
+      for (let key in params) {
+        query_params.push(`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+      }
+
+      const url = `https://nominatim.openstreetmap.org/search?${query_params.join("&")}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => callback(json))
+        .catch(() => callback());
+    },
+  };
 }
