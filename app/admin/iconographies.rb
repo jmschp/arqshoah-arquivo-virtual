8a981359c7acdacbd7a28b9c165f7d6bdeff7b09
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2022-06-19 18:32:09 +0200

    feat: add iconography admin show

--- app/admin/iconographies.rb
+++ app/admin/iconographies.rb
@@ -7,6 +7,14 @@ ActiveAdmin.register Iconography do
     { people_cited_ids: [] }
   )
 
+  controller do
+    def show
+      @iconography = Iconography.includes(versions: :item).find(params[:id])
+      @versions = @iconography.versions
+      @iconography = @iconography.versions[params[:version].to_i].reify if params[:version]
+    end
+  end
+
   index do
     selectable_column
     column :registration, sortable: :id do |iconography|
@@ -33,4 +41,73 @@ ActiveAdmin.register Iconography do
   filter :date
   filter :people_cited, label: "Pessoas citadas", multiple: true, collection: proc { Person.order(:last_name) },
                         input_html: { class: "tom-select-init" }
+
+  show do
+    h2 iconography.subtitle
+    columns do
+      column span: 3 do
+        panel "Imagens do Arquivo", class: "admin-show-image" do
+          link_to rails_blob_path(iconography.image, disposition: "inline"), target: :_blank,
+                                                                             rel: :noopener do
+            image_tag iconography.image
+          end
+        end
+      end
+      column span: 2 do
+        attributes_table do
+          row :registration
+          row :iconography_technic
+          row :iconography_type
+          row :iconography_support
+          row("Natureza do objeto") { iconography.original ? "Original" : "Réplica" }
+          row :city
+          row :state
+          row :country
+          row(:date) { iconography.date_display(:date) }
+          row :author
+          row :donor
+        end
+      end
+      column do
+        panel "Versões" do
+          render partial: "admin/shared/version"
+        end
+      end
+    end
+
+    panel Iconography.human_attribute_name(:people_cited) do
+      columns do
+        column do
+          panel Survivor.model_name.human do
+            table_for iconography.survivors_citations do
+              column :name
+            end
+          end
+        end
+        column do
+          panel Savior.model_name.human do
+            table_for iconography.savior_citations do
+              column :name
+            end
+          end
+        end
+        column do
+          panel Commoner.model_name.human do
+            table_for iconography.commoners_citations do
+              column :name
+            end
+          end
+        end
+      end
+    end
+
+    panel Archive.human_attribute_name(:description) do
+      iconography.description
+    end
+    panel Archive.human_attribute_name(:observation) do
+      iconography.observation
+    end
+
+    active_admin_comments
+  end
 end
