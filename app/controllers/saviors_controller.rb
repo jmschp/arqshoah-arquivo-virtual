41fe316feab69d7cd4bdd740613d0d6ae4f4ecd7
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2022-07-15 14:28:28 +0200

    feat: implement savior show page

--- app/controllers/saviors_controller.rb
+++ app/controllers/saviors_controller.rb
@@ -6,5 +6,11 @@ class SaviorsController < ApplicationController
     @saviors = @q.result(distinct: true).with_attached_images.page(params[:page])
   end
 
-  def show; end
+  def show
+    @savior = Savior.includes(:religion).with_attached_images
+                    .with_rich_text_description
+                    .with_rich_text_observation
+                    .with_rich_text_professional_activities
+                    .find(params[:id])
+  end
 end
--- app/models/savior.rb
+++ app/models/savior.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 class Savior < Person
+  DETAIL_ATTRIBUTES = [
+    { type: :direct, attribute: :registration },
+    { type: :direct, attribute: :name },
+    { type: :direct, attribute: :person_gender },
+    { type: :date_display, attribute: :birth_date },
+    { type: :direct, attribute: :birth_place_city },
+    { type: :direct, attribute: :birth_place_state },
+    { type: :direct, attribute: :birth_place_country },
+    { type: :date_display, attribute: :death_date },
+    { type: :direct, attribute: :birth_place_city },
+    { type: :direct, attribute: :birth_place_state },
+    { type: :direct, attribute: :birth_place_country },
+    { type: :associated, attribute: :religion }
+  ].freeze
+
   has_paper_trail skip: [:document_ts_vector], on: %i[create destroy update]
 end
